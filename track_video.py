00ffb8e013b982a100dc8b45a18da27974a0ec0b
Peter Doohan <peterdoohan0@gmail.com>
2024-05-10 13:26:58 +0000

    tracking working on linux cluster & added fn to get video_paths_df to organise sleap data processing

--- track_video.py
+++ track_video.py
@@ -1,11 +1,11 @@
 # %% imports
 import sleap
-
-sleap.disable_preallocation()  # This initializes the GPU and prevents TensorFlow from filling the entire GPU memory
-
 from pathlib import Path
+from datetime import datetime
+import pandas as pd
 
 # %% Global variables
+sleap.disable_preallocation()  # This initializes the GPU and prevents TensorFlow from filling the entire GPU memory
 
 # this folder should contain one centroid & centered instance model for each experimental session type
 SLEAP_MODELS_PATH = Path("mazeSLEAP/models")
@@ -18,9 +18,9 @@ SESSION_TYPE2SLEAP_MODEL_NAME = {
     "object_open_field_2": "C57B6_ObjectOpenField_Neuropixel-1",
 }
 
-RAW_VIDEO_PATH = Path("../data/raw_data/video")
-
-OUTPUT_SLEAP_PATH = Path("../data/preprocessed_data/SLEAP")
+# raw and preprocessed data paths
+VIDEO_PATH = Path("../data/raw_data/video")
+SLEAP_PATH = Path("../data/preprocessed_data/SLEAP")
 
 # %% Functions
 
@@ -28,19 +28,24 @@ OUTPUT_SLEAP_PATH = Path("../data/preprocessed_data/SLEAP")
 def track_video(video_path, session_type, save_labels=True, return_labels=False):
     """ """
     # load video & inference model
-    video = sleap.load_video(str(video_path))
+    video = sleap.load_video(str(video_path), grayscale=True)
     sleap_predictor = load_sleap_predictor(session_type)
     # process video
     predictions = sleap_predictor.predict(video)
     # save results
     output_filename = ".".join(video_path.name.split(".")[:-1]) + ".predicted.h5"
-    predictions.export(str(OUTPUT_SLEAP_PATH / output_filename))
+    predictions.export(str(SLEAP_PATH / output_filename))
     if return_labels:
         return predictions
 
 
 def load_sleap_predictor(session_type, batch_size=4):
-    """ """
+    """
+    Searches the models folder to find the correct models for a given session type and returns a sleap predictor object.
+
+    Args
+    - session_type: str, the type of session in your experiment (eg, maze, open_field etc.)
+    """
     all_model_paths = [p for p in SLEAP_MODELS_PATH.iterdir() if p.is_dir()]
     model_types = ["centroid", "centered_instance"]
     model_paths = []
@@ -61,3 +66,28 @@ def load_sleap_predictor(session_type, batch_size=4):
         model_paths, batch_size=batch_size, tracker="flow", tracker_max_instances=1, max_instances=1
     )
     return sleap_predictor
+
+
+def get_video_paths_df():
+    """
+    Returns a pd.Dataframe with data extracted from video filenames,
+    (rows: sessions, columns: datetime, video_path, vidoe_sync_pulse_path).
+    Will later match to pycontrol sessions with nearest datetime. Note some errors in
+    the video filenames (wrong subject, wrong session type etc. havn't been fixed in the video files
+    but datetimes should always line up so that is all we need).
+
+    note session_types for obj vect sessions are just open_field in video filenames
+    """
+    all_video_files = [f.name for f in Path(VIDEO_PATH).iterdir() if f.suffix == ".mp4"]
+    all_sync_pulse_files = [f.name for f in Path(VIDEO_PATH).iterdir() if f.suffix == ".csv"]
+    video_paths_info = []
+    for video_file in all_video_files:
+        video_paths_info.append(
+            {
+                "subject_ID": video_file.split(".")[0],
+                "datetime": datetime.strptime(video_file.split("_")[-1].split(".")[0], "%Y-%m-%d-%H%M%S"),
+                "video_path": str(Path(VIDEO_PATH) / video_file),
+            }
+        )
+    video_paths_df = pd.DataFrame(video_paths_info)
+    return video_paths_df
