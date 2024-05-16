7b5ea178ccffa49884e715dc269ecb02fb3b05e3
Peter Doohan <peterdoohan0@gmail.com>
2024-05-16 08:31:31 +0000

    ignore jobs folder & increase timelimit and memory allocation for cluster jobs

--- track_video.py
+++ track_video.py
@@ -3,9 +3,9 @@ import sleap
 from pathlib import Path
 from datetime import datetime
 import pandas as pd
+import os
 
 # %% Global variables
-sleap.disable_preallocation()  # This initializes the GPU and prevents TensorFlow from filling the entire GPU memory
 
 # this folder should contain one centroid & centered instance model for each experimental session type
 SLEAP_MODELS_PATH = Path("mazeSLEAP/models")
@@ -25,21 +25,58 @@ SLEAP_PATH = Path("../data/preprocessed_data/SLEAP")
 # %% Functions
 
 
+def run_sleap_preprocessing():
+    video_paths_df = get_video_paths_df()
+    video_paths_df = video_paths_df[~video_paths_df.tracking_completed]
+    for session_info in video_paths_df.itertuples():
+        print(f"Submitting {session_info.video_path} to HPC")
+        script_path = get_sleap_SLURM_script(session_info)
+        os.system(f"sbatch {script_path}")
+    print("Video tracking jobs submitted to HPC. Check progress with 'squeue -u <username>'")
+
+
+def get_sleap_SLURM_script(video_info):
+    session_ID = f"{video_info.subject_ID}_{video_info.session_type}_{video_info.datetime.isoformat()}"
+    script = f"""#!/bin/bash
+#SBATCH --job-name=sleap_tracking_{session_ID}
+#SBATCH --output=mazeSLEAP/jobs/out/sleap_tracking_{session_ID}.out
+#SBATCH --error=mazeSLEAP/jobs/err/sleap_tracking_{session_ID}.err
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=128GB
+#SBATCH --time=20:00:00
+
+module load miniconda
+conda activate sleap
+
+python -c "from mazeSLEAP import track_video; track_video.track_video('{video_info.video_path}', '{video_info.session_type}')"
+"""
+    script_path = f"mazeSLEAP/jobs/slurm/sleap_tracking_{session_ID}.sh"
+    with open(script_path, "w") as f:
+        f.write(script)
+
+    return script_path
+
+
 def track_video(video_path, session_type, save_labels=True, return_labels=False):
     """ """
     # load video & inference model
     video = sleap.load_video(str(video_path), grayscale=True)
+    print(f"tracking video {video_path}")
     sleap_predictor = load_sleap_predictor(session_type)
     # process video
     predictions = sleap_predictor.predict(video)
     # save results
-    output_filename = ".".join(video_path.name.split(".")[:-1]) + f".predicted_{datetime.now().isoformat()}.h5"
-    predictions.export(str(SLEAP_PATH / output_filename))
+    output_filename = ".".join(Path(video_path).name.split(".")[:-1]) + f".predicted_{datetime.now().isoformat()}.h5"
+    if save_labels:
+        predictions.export(str(SLEAP_PATH / output_filename))
     if return_labels:
         return predictions
 
 
-def load_sleap_predictor(session_type, batch_size=4):
+def load_sleap_predictor(session_type, batch_size=16):
     """
     Searches the models folder to find the correct models for a given session type and returns a sleap predictor object.
 
@@ -63,7 +100,7 @@ def load_sleap_predictor(session_type, batch_size=4):
         else:
             model_path = model_paths.append(str(model_path[0]))
     sleap_predictor = sleap.load_model(
-        model_paths, batch_size=batch_size, tracker="flow", tracker_max_instances=1, max_instances=1
+        model_paths, batch_size=batch_size, tracker="flow", tracker_max_instances=1, max_instances=1, progress_reporting='json'
     )
     return sleap_predictor
 
@@ -82,7 +119,7 @@ def get_video_paths_df():
     all_video_files = [f.name for f in Path(VIDEO_PATH).iterdir() if f.suffix == ".mp4"]
     all_sleap_files = [f.name for f in Path(SLEAP_PATH).iterdir() if f.suffix == ".h5"]
     all_sleap_original_datetime_strings = [  # datetimes_strings of original videos predictions were made on
-        f.split(".")[1].split("_")[-3] for f in all_sleap_files
+        f.split(".")[1].split("_")[-1] for f in all_sleap_files
     ]
     video_paths_info = []
     for video_file in all_video_files:
